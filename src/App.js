6b5075e1d19388b6fedb8251e2b4c40b40bc074b
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-09-13 17:22:32 -0500

    Add Routes for when a user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import Container from './components/Container';
-import Sidebar from './components/SidebarNav';
+import { useState } from 'react';
 import Wrapper from './components/Wrapper';
-import CategoryPage from './pages/CategoryPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import styled from '@emotion/styled';
-
-const StyledMain = styled.main`
-  flex: 1;
-`;
+import LoggedInRoutes from './routes/LoggedInRoutes';
+import LoggedOutRoutes from './routes/LoggedOutRoutes';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <Wrapper>
-      <Container>
-        <Sidebar />
-        <StyledMain>
-          <Routes>
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
-            <Route path='/' element={<CategoryPage />} />
-            <Route path='/categories' element={<CategoryPage />} />
-            <Route path='/categories/:type' element={<CategoryPage />} />
-            <Route path='/transactions' element={<h1>Transactions</h1>} />
-            <Route path='/budgets' element={<h1>Budgets</h1>} />
-            <Route path='*' element={<h1>Not Found</h1>} />
-          </Routes>
-        </StyledMain>
-      </Container>
+      {user ? (
+        <LoggedInRoutes user={user} setUser={setUser} />
+      ) : (
+        <LoggedOutRoutes />
+      )}
     </Wrapper>
   );
 }
---
+++ src/routes/LoggedInRoutes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Sidebar from '../components/SidebarNav';
+import Container from '../components/Container';
+import CategoryPage from '../pages/CategoryPage';
+import styled from '@emotion/styled';
+
+const StyledMain = styled.main`
+  flex: 1;
+`;
+
+function LoggedInRoutes() {
+  return (
+    <Container>
+      <Sidebar />
+      <StyledMain>
+        <Routes>
+          <Route index path='/' element={<CategoryPage />} />
+          <Route path='/categories' element={<CategoryPage />} />
+          <Route path='/categories/:type' element={<CategoryPage />} />
+          <Route path='/transactions' element={<h1>Transactions</h1>} />
+          <Route path='/budgets' element={<h1>Budgets</h1>} />
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </StyledMain>
+    </Container>
+  );
+}
+
+export default LoggedInRoutes;
